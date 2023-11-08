f17b7519688861389107d8175123d2e651582187
DarkMortal <chodab74@gmail.com>
2023-11-08 23:34:57 +0530

    Added vercel configurations

--- db.js
+++ db.js
@@ -65,43 +65,6 @@ async function getAllBooks(){
     });
 }
 
-/*createBook({
-    id: 'id2',
-    name: "Harry Potter",
-    author: "Saptarshi Dey",
-    description: "Idk",
-    genres: ['horror', 'adventure', 'thriller']
-}).then(() => console.log("Data inserted"))
-.catch(err => {
-    let actualError = JSON.parse(JSON.stringify(err));
-    if (actualError.code == 'P2002') console.log("Id already present");
-    else console.error(err);
-});*/
-
-/*updateBook('id2', {
-    name: "Adeventues of Sherlock Holmes",
-    author: "Sir Arthur Conan Doyle"
-}).then((data) => console.log("Data updated", data))
-.catch(err => {
-    console.error(err)
-});*/
-
-/*deleteBook('id1')
-.then(() => console.log("Data deleted"))
-.catch(err => {
-    let actualError = JSON.parse(JSON.stringify(err));
-    if (actualError.code == 'P2025') console.log("Record doesn't exist");
-    else console.error(err);
-})
-
-getBookData('id2')
-.then(data => console.log(data))
-.catch(err => console.error(err))
-
-getAllBooks()
-.then(data => console.log(data))
-.catch(err => console.error(err))*/
-
 module.exports = {
     createBook, deleteBook, updateBook, getBookData, getAllBooks
 }
